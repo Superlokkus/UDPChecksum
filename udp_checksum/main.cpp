25901c43a8d304cfec3480887b1b3d434c435100
Markus Klemm <markus@markusklemm.net>
2014-11-12 09:48:22 +0100

    Initial Commit

---
+++ udp_checksum/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  udp_checksum
+//
+//  Created by Markus Klemm on 12.11.14.
+//  Copyright (c) 2014 net.markusklemm. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
